ede0f872bcf4dacb293ad039a7b990cb579d7fff
sukhmandeep91 <khalsa131352@gmail.com>
2025-06-28 22:30:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -42,14 +42,6 @@ const statusData = [
     }
 ];
 
-```let likeCount = 0;
-
-function likePost() {
-  likeCount++;
-  document.getElementById("like-count").innerHTML = likeCount;
-  document.getElementById("like-button").innerHTML = "Liked!";
-  document.getElementById("like-button").disabled = true;
-}```
 // DOM Elements
 const statusGrid = document.querySelector('.status-grid');
 const loadMoreBtn = document.getElementById('loadMore');
